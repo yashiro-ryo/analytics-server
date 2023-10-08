b14ea02980034ba01ebc5424a5f726170e022f65
yashiroryo <>
2023-10-08 16:08:41 +0900

    feat: POST /api/v1/registerエンドポイントのテストを追加

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use axum::{
     routing::{get, post},
     Json, Router,
 };
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use std::env;
 use std::net::SocketAddr;
 
@@ -16,7 +16,6 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     let app = create_app();
-        
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
 
@@ -45,11 +44,50 @@ async fn register_event(Json(payload): Json<Event>) -> impl IntoResponse {
         event_name: payload.event_name,
         event_detail: payload.event_detail,
     };
-    (StatusCode::CREATED, Json(event));
+    (StatusCode::CREATED, Json(event))
 }
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize, Debug, PartialEq, Eq)]
 struct Event {
     event_name: String,
     event_detail: String,
+}
+
+// ここからテスト
+#[cfg(test)]
+mod test {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{header, Method, Request},
+    };
+    use tower::ServiceExt;
+
+    // POST /api/v1/register のテスト
+    #[tokio::test]
+    async fn should_return_event() {
+        let req = Request::builder()
+            .uri("/api/v1/register")
+            .method(Method::POST)
+            .header(header::CONTENT_TYPE, mime::APPLICATION_JSON.as_ref())
+            .body(Body::from(
+                r#"{ "event_name": "button-click", "event_detail": "run-button" }"#,
+            ))
+            .unwrap();
+
+        let res = create_app().oneshot(req).await.unwrap();
+
+        let bytes = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let body: String = String::from_utf8(bytes.to_vec()).unwrap();
+        println!("{}", body);
+        let event: Event = serde_json::from_str(&body).expect("cannot convert Event instance.");
+
+        assert_eq!(
+            event,
+            Event {
+                event_name: "button-click".to_string(),
+                event_detail: "run-button".to_string(),
+            }
+        );
+    }
 }
