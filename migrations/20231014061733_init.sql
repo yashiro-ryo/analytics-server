d58db6d4667d4471424f09b2a0d629e6127d2861
yashiroryo <>
2023-10-25 01:18:25 +0900

    chore: DBのmigrationファイルを修正

--- migrations/20231014061733_init.sql
+++ migrations/20231014061733_init.sql
@@ -1,7 +1,8 @@
 CREATE TABLE events_table
 (
-  uid       VARCHAR(20)   PRIMARY KEY NOT NULL,
-  name      VARCHAR(20)   NOT NULL,
-  body      TEXT          NOT NULL,
-  timestamp TIMESTAMPTZ
+  event_id      serial        PRIMARY KEY,
+  uid           VARCHAR(20)   NOT NULL,
+  event_name    VARCHAR(50)   NOT NULL,
+  event_detail  TEXT          NOT NULL,
+  timestamp     TIMESTAMPTZ
 );
